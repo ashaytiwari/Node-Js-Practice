025061cbba76688873061b591c7cd8f904b3aa98
ashaytiwari <ashaytiwari2001@gmail.com>
2022-05-20 15:41:53 +0530

    feat: express router and body parser implemented for admin and shop routes

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(8000);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(8000);
+
+// additional configuration for nodemon so address already in use error didn't stuck
+process.once('SIGUSR2', function () {
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, 'SIGINT');
+});
 
 console.log('server is listening on port 8000');
