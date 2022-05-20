62bb809bb05e0c028556657c42231acba1d38dd7
ashaytiwari <ashaytiwari2001@gmail.com>
2022-05-20 15:58:13 +0530

    feat: filtered admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (request, response, next) => {
-  response.send('<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit">Add product</button></form>');
+  response.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /> <button type="submit">Add product</button></form>');
 });
 
-router.post('/product', (request, response, next) => {
+router.post('/add-product', (request, response, next) => {
   console.log(request.body);
   response.redirect('/');
 });
