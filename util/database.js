8373d88acd68ff9eddc7247c8eb2781bd9ff2175
ashaytiwari <ashaytiwari2001@gmail.com>
2022-05-31 11:30:58 +0530

    feat: sequelize setup done

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'ONLINEBOOKSTORE',
-  password: 'Ashay@123'
+const sequelize = new Sequelize('ONLINEBOOKSTORE', 'root', 'Ashay@123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
