686bc7e2ad4da51358f1c508fb6870350be893f3
ashaytiwari <ashaytiwari2001@gmail.com>
2022-08-12 18:16:56 +0530

    refactor: removed unused models file and feat: implemented add to cart functionality via custom schema method

--- models/user.js
+++ models/user.js
@@ -28,6 +28,34 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function (product) {
+
+  let productIndex = -1;
+
+  if (this.cart.items.length !== 0) {
+    productIndex = this.cart.items.findIndex((_product) => {
+      return _product.productId.toString() === product._id.toString();
+    });
+  }
+
+  const updatedCartItems = [...this.cart.items];
+  let quantity = 1;
+
+  if (productIndex === -1) {
+    updatedCartItems.push({ productId: product._id, quantity });
+  } else {
+    quantity = this.cart.items[productIndex].quantity + 1;
+    updatedCartItems[productIndex].quantity = quantity;
+  }
+
+  const updatedCart = { items: updatedCartItems };
+
+  this.cart = updatedCart;
+
+  return this.save();
+
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
